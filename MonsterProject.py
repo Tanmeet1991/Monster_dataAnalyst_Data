884f39283c33bef39c5b1b8ca18f30444ffc7bf7
Tanmeet singh <90331925+Tanmeet1991@users.noreply.github.com>
2023-06-18 13:06:32 +0530

    Update MonsterProject.py
    Due to changes in the website all together, updated the code and ready to use.

--- MonsterProject.py
+++ MonsterProject.py
@@ -13,22 +13,24 @@ class Web:
         self.driver = webdriver.Chrome(executable_path="d:\\Python Practice tests\\Web scraping\\chromedriver.exe")
         self.driver.get('https://www.monsterindia.com/')
         time.sleep(3)
-
+        self.driver.maximize_window()
+        time.sleep(2)
+        
 class Login_Monster(Web):
     def login(self):
         '''This function will do the login process, with email id and password, and will print 'Login into Monster site:- Successful.',
-        else it will give the error if any.
+        else it will the error if any.
         '''
         try:
-            self.login_button = self.driver.find_element(By.XPATH, "//div[@class='nav-wrapper flex-container slide-effect']//child::ul[@class='mq-menu-list user-section flex-item']//child::ul//child::li//child::a//child::span[text()= 'Jobseeker Login']")
+            self.login_button = self.driver.find_element(By.XPATH, "/html/body/div[5]/header/header/div[2]/div[2]/div/div[2]/ul/li[1]/a/span")
             self.login_button.click()
             time.sleep(3)    
-            self.email = self.driver.find_element(By.XPATH, "//input[@id='signInName']")
+            self.email = self.driver.find_element(By.XPATH, "//*[@id='signInName']")
             self.email.send_keys(logindetails.login.get_email())
             time.sleep(2)
-            self.password = self.driver.find_element(By.XPATH, "//input[@name='password']")
+            self.password = self.driver.find_element(By.XPATH, "//*[@id='password']")
             self.password.send_keys(logindetails.login.get_password())
-            self.signIn = self.driver.find_element(By.XPATH, "//input[@id='signInbtn']")
+            self.signIn = self.driver.find_element(By.XPATH, "//*[@id='signInbtn']")
             self.signIn.click()
             print('Login into Monster site:- Successful.')
             time.sleep(3)
@@ -38,10 +40,10 @@ class Login_Monster(Web):
     
 class Job_description(Login_Monster):
     profile = 'Data Analyst'
-    location_1 = 'Gurgaon'
+    location_1 = 'Gurugram'
     def Job_profile(self):
-        ''' Job profile function will help in searching for the required profile and location, wherever you need and whatever you are looking
-        for. You can change location and profile, under profile = ''. and location_1 = ''. 
+        ''' Job profile function will hlp in searching for the required profile and location, wherever you need and whatever you are looking
+        for. You can hanfe location and profile, under profile = ''. and location_1 = ''. 
         '''
         self.search = self.driver.find_element(By.XPATH, "//input[@class= 'input search-bar home_ac']")
         self.search.click()
@@ -51,6 +53,8 @@ class Job_description(Login_Monster):
         self.location.send_keys(self.location_1)
         self.search_btn = self.driver.find_element(By.XPATH, "//input[@class='btn']")
         self.search_btn.click()
+        time.sleep(3)
+        
         return
 
 class Data(Job_description):
@@ -61,38 +65,43 @@ class Data(Job_description):
         self.df = pd.DataFrame({'Job_Title':[''], 'Company':[''], 'Experience':[''], 'Salary':[''], 'Job_Description':[''], 'Post_Date':[''], 'Link':['']})
         
         while True:
-            self.scroll_down_1 = self.driver.execute_script('window.scrollTo(0, 600)')
+            self.scroll_down_1 = self.driver.execute_script('window.scrollTo(0, 2000)')
             time.sleep(3)
-            self.scroll_down_2 = self.driver.execute_script('window.scrollTo(600, 1000)')
-            time.sleep(3)
-            self.scroll_down_3 = self.driver.execute_script('window.scrollTo(1000, 1500)')
+
+            self.scroll_down_2 = self.driver.execute_script('window.scrollTo(2000, 3300)')
             time.sleep(3)
             self.soup = BeautifulSoup(self.driver.page_source, 'lxml')
-            self.postings = self.soup.find_all('div', class_='card-panel apply-panel job-apply-card')
+            self.postings = self.soup.find_all('div', class_='cardContainer')
             for self.post in self.postings:
                 try:
-                    self.job_title = self.post.find('h3', class_='medium').text.strip()
-                    self.company = self.post.find('span', class_='company-name').text.strip()
                     try:
-                        self.experience_r = self.post.find('i', class_='mqfi-bag').text.strip()
+                        self.job_title = self.post.find('div', class_='jobTitle').text.strip()
+                    except:
+                        print("job_title_error")
+                    try:
+                        self.company = self.post.find('div', class_='companyName').text.strip()
+                    except:
+                        print("company_name_error")
+                    try:
+                        self.experience_r = self.post.find('i', class_='mqfisrp-briefcase').text.strip()
                     except:
                         self.experience_r = 'N/A'
-                    self.salary = self.post.find('i', class_='mqfi-coin-stack fs-18 iSal').text.strip()
+
                     try:
-                        self.job_descrip = self.post.find('p', class_='job-descrip').text.strip()
+                        
+                        self.post_date = self.post.find('p', class_='timeText').text.strip()
                     except:
-                        self.job_descrip = 'N/A'
-                    self.post_date = self.post.find('span', class_='posted').text.strip()
-                    self.link = self.post.find('h3', class_='medium').find('a').get('href')
-                    self.df = self.df.append({'Job_Title':self.job_title, 'Company':self.company, 'Experience':self.experience_r, 'Salary':self.salary, 'Job_Description':self.job_descrip, 'Post_Date':self.post_date, 'Link':self.link}, ignore_index=True)
+                        print("post_date_error")
+                    self.df = self.df.append({'Job_Title':self.job_title, 'Company':self.company, 'Experience':self.experience_r, 'Post_Date':self.post_date}, ignore_index=True)
                 except Exception as e:
                     print(e, 'There is an Error:')
             try:
-                self.next_page = self.driver.find_element(By.XPATH, "//button[contains(text(), 'Next')]")
+                self.next_page = self.driver.find_element(By.CLASS_NAME, 'mqfisrp-right-arrow')
                 self.next_page.click()
+                time.sleep(4)
             except:
                 break
-            self.postings = self.soup.find_all('div', class_='card-panel apply-panel job-apply-card')  
+            self.postings = self.soup.find_all('div', class_='cardContainer')  
             self.df.to_csv('D:\\Monster Project\\Monster_Data_Anayst.csv')
 
 login_M = Data()
